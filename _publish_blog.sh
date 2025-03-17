4839783fbc46c58682bd34ecbe01248cae4a7129
Connor Duncan <connor@connorduncan.xyz>
2025-03-17 07:22:02 -0500

    First stab at static syntax highlighting

--- _publish_blog.sh
+++ _publish_blog.sh
@@ -1,1 +1,10 @@
-ssg _blog/ blog/ "Connor Duncan\'s Blog" 'https://www.connorduncan.xyz/blog/'
+mkdir _blog_intermediate/
+cp _blog/**.html ./_blog_intermediate
+
+for file in ./_blog/**.md; do
+	echo $file
+	mdhl --file $file > ./_blog_intermediate/$(basename $file)
+done
+ls _blog_intermediate
+ssg _blog_intermediate/ blog/ "Connor Duncan\'s Blog" 'https://www.connorduncan.xyz/blog/'
+rm -rf _blog_intermediate/
