1ab57facc13aac55611768294255c96918b251f8
Connor Duncan <connor@connorduncan.xyz>
2025-04-13 16:12:49 -0500

    switch to tui style inprogress

--- _publish_blog.sh
+++ _publish_blog.sh
@@ -3,7 +3,7 @@ cp _blog/**.html ./_blog_intermediate
 
 for file in ./_blog/**.md; do
 	echo $file
-	mordant --file $file -c ./_mordant.toml > ./_blog_intermediate/$(basename $file)
+	mordant $file -c ./_mordant.toml > ./_blog_intermediate/$(basename $file)
 done
 ls _blog_intermediate
 ssg _blog_intermediate/ blog/ "Connor Duncan\'s Blog" 'https://www.connorduncan.xyz/blog/'
