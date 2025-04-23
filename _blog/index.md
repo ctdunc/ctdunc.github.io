a98b4686cb91aab5f38d9508bdc970952d53400a
Connor Duncan <connor@connorduncan.xyz>
2025-04-23 16:47:22 -0500

    Use significantly improved mordant syntax highlights, add mordant post

--- _blog/index.md
+++ _blog/index.md
@@ -4,6 +4,7 @@ title: Thoughts
 ---
 Listed in the order I feel like listing them in.
 
+- [mordant: actually static web syntax highlighting](./mordant.html)
 - [enabling LSP support for `clientside_callbacks` in dash](./dash-clientside-lsp.html)
 - [`{vsc,nvim}-dash` are plugins now](./dash-clientside-treesitter-ez-mode.html)
 - [fixing dash `clientside_callbacks` with `treesitter`](./dash-clientside-treesitter.html)
--- _publish_blog.sh
+++ _publish_blog.sh
@@ -1,8 +1,13 @@
+# make a temp directory for the mordant-ified md files.
 mkdir _blog_intermediate/
-cp _blog/**.html ./_blog_intermediate
 
 for file in ./_blog/**.md; do
 	mordant $file -c ./_mordant.toml > ./_blog_intermediate/$(basename $file)
-	lowdown --template=./_template.html -s ./_blog_intermediate/$(basename $file) -thtml --html-no-skiphtml --html-no-escapehtml > ./blog/$(basename $file .md).html
+	# we need the no skip/escape html options, since we want to use inline tags.
+	lowdown --template=./_template.html \
+	  -s ./_blog_intermediate/$(basename $file) \
+	  -thtml \
+	  --html-no-skiphtml \
+	  --html-no-escapehtml > ./blog/$(basename $file .md).html
 done
 rm -rf _blog_intermediate/
